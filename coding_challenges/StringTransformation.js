8eb8c6d5a06a248cf85b1138bc14a68331e814a0
pinsoegustave <ntwalimudasubirag@gmail.com>
2024-06-09 05:59:47 +0200

    codes edit

--- coding_challenges/StringTransformation.js
+++ coding_challenges/StringTransformation.js
@@ -1,26 +1,26 @@
 function StringTransformation(input) {
     const length = input.length;
-    let result = '';
+    let answer = '';
 
     if (length % 3 === 0) {
-        result = input.split('').reverse().join('');
+        answer = input.split('').reverse().join('');
     }
 
     if (length % 5 === 0) {
-        result = input.split('').map(char => char.charCodeAt(0)).join(' ');
+        answer = input.split('').map(char => char.charCodeAt(0)).join(' ');
     }
     if (length % 15 === 0) {
-        result = input.split('').reverse().join('');
-        result = result.split('').map(char => char.charCodeAt(0)).join(' ');
+        answer = input.split('').reverse().join('');
+        answer = answer.split('').map(char => char.charCodeAt(0)).join(' ');
     }
-    if (result === '') {
-        result = input;
+    if (answer === '') {
+        answer = input;
     }
 
-    return result;
+    return answer;
 }
 
-//Test
+
 console.log(StringTransformation("Hamburger"));
 console.log(StringTransformation("Pizza"));
 console.log(StringTransformation("Chocolate Chip Cookie"));
